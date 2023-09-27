93f47ddf7d58f9d3278dbd89c588461fb9e52c2f
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-09-27 16:46:18 +0530

    Home mouse Icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import HomeBelow from './Components/HomeBelow';
 
 function App() {
   return (
    <div>
     <Header/>
     <Home/>
+    {/* <HomeBelow/> */}
    </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,8 @@ import '../Css/Home.css';
 import logo from '../images/background.jpg';
 
 
+
+
 function Home() {
   return (
     <div className='home'>
@@ -10,41 +12,48 @@ function Home() {
       
     {/* <img  className='home-image' src={logo} alt="img" /> */}
     
-    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-inner">
-  <img src={logo} className="home-image d-block w-100" alt="..."/>
-    <div className="carousel-item active">
-      {/* <img src={logo} className="d-block w-100" alt="..."/> */}
-      <div className="carousel-caption d-md-block">
-        <h1>BANKING SOLUTIONS</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cupiditate suscipit, magnam libero velit esse sapiente officia inventore!.</p>
+          <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+        <div className="carousel-inner">
+        <img src={logo} className="home-image d-block" alt="..."/>
+          <div className="carousel-item active">
+            {/* <img src={logo} className="d-block w-100" alt="..."/> */}
+            <div className="carousel-caption d-md-block">
+              <h1>BANKING SOLUTIONS</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cupiditate suscipit, magnam libero velit esse sapiente officia inventore!.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            {/* <img src={logo} className="d-block w-100" alt="..."/> */}
+            <div className="carousel-caption d-md-block">
+              <h1>FINANCING SOLUTIONS</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cupiditate suscipit, magnam libero velit esse sapiente officia inventore!</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            {/* <img src={logo} className="d-block w-100" alt="..."/> */}
+            <div className="carousel-caption d-md-block">
+              <h1>SAVINGS ACCOUNT</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cupiditate suscipit, magnam libero velit esse sapiente officia inventore!</p>
+            </div>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-    </div>
-    <div className="carousel-item">
-      {/* <img src={logo} className="d-block w-100" alt="..."/> */}
-      <div className="carousel-caption d-md-block">
-        <h1>FINANCING SOLUTIONS</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cupiditate suscipit, magnam libero velit esse sapiente officia inventore!</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      {/* <img src={logo} className="d-block w-100" alt="..."/> */}
-      <div className="carousel-caption d-md-block">
-        <h1>SAVINGS ACCOUNT</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cupiditate suscipit, magnam libero velit esse sapiente officia inventore!</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
 
+      <div className="mouse smoothscroll" href="#next"> 
+        <div className="mouse-icon">
+          <div className="mouse-wheel">
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
